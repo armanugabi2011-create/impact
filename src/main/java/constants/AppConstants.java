3fe42aeca8c86c6294359cd40b8b40c44f9988da
armanugabi2011 <armanugabi2011@gmail.com>
2026-01-29 16:57:34 +0200

    First initial commit

---
+++ src/main/java/constants/AppConstants.java
@@ -0,0 +1,7 @@
+package com.impact.lessons.constants;
+
+public class AppConstants {
+    public AppConstants(){}
+    public static final String HelloWorld = "Hello World";
+
+}
---
+++ src/main/java/services/HelloService.java
@@ -0,0 +1,8 @@
+package com.impact.lessons.services;
+import com.impact.lessons.constants.AppConstans;
+public class HelloService {
+    AppConstans
+}
+
+public class HelloService {
+}
